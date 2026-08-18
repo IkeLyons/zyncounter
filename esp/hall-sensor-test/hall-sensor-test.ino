4a66e606ae74b036403127bc638e0173af744e1f
IkeLyons <ike.lyons.ny@gmail.com>
2026-08-18 18:54:45 -0400

    Refactor detection block

--- esp/hall-sensor-test/hall-sensor-test.ino
+++ esp/hall-sensor-test/hall-sensor-test.ino
@@ -3,25 +3,25 @@
 #include <BLEServer.h>
 #include <BLE2902.h>
 
-#define SERVICE_UUID          "96bde720-973d-4f43-820b-0cd2ff8b666c"
-#define CHARACTERISTIC_UUID   "d5c94e7e-47e3-484d-897a-ea417b91b77a"
+#define SERVICE_UUID "96bde720-973d-4f43-820b-0cd2ff8b666c"
+#define CHARACTERISTIC_UUID "d5c94e7e-47e3-484d-897a-ea417b91b77a"
 
 const int ledPin = 2;
 const int hallSensorPin = 27;
 const int buttonPin = 26;
+int highCount = 0;
 
 BLECharacteristic *pCharacteristic;
 int lastSensorState = -1;
 
-class ServerCallbacks: public BLEServerCallbacks {
+class ServerCallbacks : public BLEServerCallbacks {
   void onDisconnect(BLEServer *pServer) {
     Serial.println("Client disconnected, resuming advertising");
     pServer->startAdvertising();
   }
 };
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 
   BLEDevice::init("Zyncounter");
@@ -30,8 +30,7 @@ void setup()
   BLEService *pService = pServer->createService(SERVICE_UUID);
   pCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY
-  );
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
   pCharacteristic->addDescriptor(new BLE2902());
 
   pCharacteristic->setValue("Magnet Off");
@@ -50,29 +49,35 @@ void setup()
   Serial.println("Hall Effect Sensor Test");
 }
 
-void loop()
-{
+void loop() {
   int sensorState = digitalRead(hallSensorPin);
 
-  if(sensorState != lastSensorState) {
-    lastSensorState = sensorState;
-
-    if(sensorState == LOW) {
+  if (sensorState == LOW) {
+    highCount = 0;
+    digitalWrite(ledPin, HIGH);
+    pCharacteristic->setValue("Magnet On");
+    if (sensorState != lastSensorState) {
       Serial.println("Magnet Detected");
-      digitalWrite(ledPin, HIGH);
-      pCharacteristic->setValue("Magnet On");
-    } else {
+      pCharacteristic->notify();
+    }
+    lastSensorState = 0;
+  } else {
+    highCount++;
+    if (highCount == 0) {
+      Serial.println("First detected magnet absence");
+    }
+    if (highCount == 5) {
       Serial.println("No Magnet");
       digitalWrite(ledPin, LOW);
       pCharacteristic->setValue("Magnet Off");
+      pCharacteristic->notify();
     }
+    lastSensorState = 1;
+  } 
 
-    pCharacteristic->notify();
-  }
-
-  if(digitalRead(buttonPin) == LOW){
+  if (digitalRead(buttonPin) == LOW) {
     Serial.println("Button Pressed");
   }
 
-  delay(500);
+  delay(100);
 }
